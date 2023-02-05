4671e07f02280af1c3ce05059da70e1a383c378a
ZakAttakk <zjbrewer@gmail.com>
2023-02-05 15:06:44 -0500

    Update Deck.java

--- src/game/Deck.java
+++ src/game/Deck.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public interface Deck {
 	public void shuffle();
-//	public ArrayList<BasicCard> createP1Hand();
-//	public ArrayList<BasicCard> createP2Hand();
 	public BasicCard draw();
 	public ArrayList<BasicCard> getDeck();
 }
